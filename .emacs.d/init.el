225c06189b30d5a2159318c214697d6d09d42e77
chunyoupeng <149676664+chunyoupeng@users.noreply.github.com>
2023-12-17 23:38:02 +0800

    Update gc init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,24 @@
 ;; NOTE: init.el is now generated from Emacs.org.  Please edit that file
 ;;       in Emacs and init.el will be generated automatically!
 
+
+;; Adjust garbage collection thresholds during startup, and thereafter
+
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+
+;; Process performance tuning
+
+(setq read-process-output-max (* 4 1024 1024))
+(setq process-adaptive-read-buffering nil)
+
+
+;; Bootstrap config
+
 ;; You will most likely need to adjust this font size for your system!
 (defvar efs/default-font-size 150)
 (defvar efs/default-variable-font-size 150)
