d73d87448f3a422247efab522378ba4656c7ecff
chunyoupeng <chunyoupeng@gmail.com>
2024-10-13 20:18:56 +0800

    Remove some unused command from shell

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,10 +1,8 @@
-alias br="broot"
 alias shut="sudo shutdown -h now"
 alias ls="exa --color=auto"
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='exa -lah -S  --color=auto --group-directories-first'
-alias e="emacsclient"
 alias gmm="git commit -m"
 alias gam="gaa; gcm"
 alias gd="git diff"
@@ -12,22 +10,17 @@ alias gt="git status"
 alias gaa="git add --all"
 alias ga="git add"
 alias gg="git lg"
-alias copy='xclip -selection clipboard'
-alias antlr4="java -jar /usr/local/lib/antlr-4.13.1-complete.jar"
-alias grun="java org.antlr.v4.gui.TestRig"
-alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
-alias grun='java -Xmx500M -cp "/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
-# Add an "alert" alias for long running commands.  Use like so:
-#   sleep 10; alert
 alias cl="clear"
 alias cd..="cd .."
 alias pp="python -m pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org "
-ec() {
-    emacsclient "$(find . -type f | fzf)"
-}
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     alias cat=bat
 else
+    alias copy='xclip -selection clipboard'
+    alias antlr4="java -jar /usr/local/lib/antlr-4.13.1-complete.jar"
+    alias grun="java org.antlr.v4.gui.TestRig"
+    alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
+    alias grun='java -Xmx500M -cp "/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
     alias cat=batcat
 fi
--- shell/path.sh
+++ shell/path.sh
@@ -1,18 +1,12 @@
 # General exports
 export CLASSPATH=".:/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH"
-export CXX=/usr/bin/g++
-export GTK_IM_MODULE=ibus
-export XMODIFIERS=@im=ibus
-export QT_IM_MODULE=ibus
+# export CXX=/usr/bin/g++ 
 export LANG=zh_CN.UTF-8
 export LC_ALL=zh_CN.UTF-8
 export http_proxy=http://127.0.0.1:7890
 export https_proxy=http://127.0.0.1:7890
-export REDIS_URL=redis://localhost:6379
 export GLOBAL_AGENT_ENVIRONMENT_VARIABLE_NAMESPACE=http://127.0.0.1:7890
-export LLAMA_CUBLAS=1
 export USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
-export PATH=/usr/local/cuda-12.2/bin:$PATH
 export HOMEBREW_NO_AUTO_UPDATE=1
 
 export NVM_DIR="$HOME/.nvm"
@@ -22,7 +16,6 @@ export NVM_DIR="$HOME/.nvm"
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 eval "$(zoxide init zsh)"
-# eval "$(starship init zsh)"
 eval $(thefuck --alias)
 . "$HOME/.cargo/env"
 
